6d69eeedbdef94dcd4853c79ef36069377bdbfb6
FengCastelo <ianfcastelo7@gmail.com>
2026-03-06 14:06:45 -0300

    Implementing reactive forms with validation

--- src/app/categories/category/category.component.ts
+++ src/app/categories/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-category',
@@ -7,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './category.component.scss'
 })
 export class CategoryComponent {
+  formField: FormGroup;
+  
+  constructor() {
+    this.formField = new FormGroup({
+       name: new FormControl('', [Validators.required, Validators.maxLength(60)]),
+       description: new FormControl('', Validators.required)
+      });
+  }
 
+  save() {
+    this.formField.markAllAsTouched();
+
+    if(this.formField.valid) {
+        console.log('data: ', this.formField.value);
+    }
+  }
+
+  isFieldInvalid(fieldName: string) : boolean {
+    const field = this.formField.get(fieldName);
+    return field?.invalid && field.touched && field?.errors?.['required'];
+
+  }
 }
