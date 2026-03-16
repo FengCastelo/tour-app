8a05170be2a669c55f49cdeeb5fc4dd7a1c19ca8
FengCastelo <ianfcastelo7@gmail.com>
2026-03-15 22:32:47 -0300

    Creating a category service to consume the API

---
+++ src/app/categories/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from './category';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  private baseUrl: string = 'http://localhost:3000/categories';
+
+  constructor(private http: HttpClient) {}
+
+  save(category: Category): Observable<Category> {
+    return this.http.post<Category>(this.baseUrl, category);
+  }
+
+  findAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl)
+  }
+}
