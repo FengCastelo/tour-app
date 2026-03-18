a10ff6b6a247947f7223a9bd6bb376e8c5c84715
FengCastelo <ianfcastelo7@gmail.com>
2026-03-18 20:24:00 -0300

    Creating gallery page with the places cards and details

--- src/app/gallery/gallery/gallery.component.ts
+++ src/app/gallery/gallery/gallery.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Place } from '../../places/Place';
+import { Category } from '../../categories/category';
+import { PlaceService } from '../../places/place.service';
+import { CategoryService } from '../../categories/category.service';
 
 @Component({
   selector: 'app-gallery',
   standalone: false,
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  styleUrl: './gallery.component.scss',
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+  places: Place[] = [];
+  categoriesFilter: Category[] = [];
 
+  constructor(
+    private placeService: PlaceService,
+    private categoryService: CategoryService,
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryService
+      .findAll()
+      .subscribe((categories) => (this.categoriesFilter = categories));
+
+    this.placeService
+      .findAll()
+      .subscribe((placesResponse) => (this.places = placesResponse));
+  }
 }
