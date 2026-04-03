91e08c426f3d429ac86ea3cb9f94a3324bde3aa1
FengCastelo <ianfcastelo7@gmail.com>
2026-04-03 14:24:28 -0300

    Creating the application's home page

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Profile } from './profile.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
   standalone: false,
   templateUrl: './landingpage.component.html',
-  styleUrl: './landingpage.component.scss'
+  styleUrl: './landingpage.component.scss',
 })
 export class LandingpageComponent {
+  profile: Profile | undefined;
 
+  constructor(private router: Router) {
+    
+  }
+
+  navigate() {
+    this.router.navigate(['/pages/gallery']);
+  }
+
+  loginWithGoogle() {
+
+  }
+
+  isLoggedIn(): boolean{
+    return !!this.profile;
+  }
 }
